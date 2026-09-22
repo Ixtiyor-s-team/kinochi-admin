f578b0d9566540727a323462dcb982f19e5855f7
saitovixtiyor <ixtyorsaitov@gmail.com>
2026-09-22 20:45:34 +0500

    loading page

--- src/pages/loading/page.tsx
+++ src/pages/loading/page.tsx
@@ -1,3 +1,14 @@
+import { Flex, LoadingOverlay } from "@mantine/core";
+
 export default function LoadingPage() {
-  return <div>LoadingPage</div>;
+  return (
+    <Flex w={"100%"} h={"100vh"} align={"center"} justify={"center"}>
+      <LoadingOverlay
+        visible
+        zIndex={1000}
+        overlayProps={{ radius: "sm", blur: 2 }}
+        loaderProps={{ type: "dots", size: "xl" }}
+      />
+    </Flex>
+  );
 }
