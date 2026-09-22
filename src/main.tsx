ad49360cd324459c24659ced3fbaaeb11137eda9
saitovixtiyor <ixtyorsaitov@gmail.com>
2026-09-22 13:14:39 +0500

    mantine ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MantineProvider } from "@mantine/core";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
