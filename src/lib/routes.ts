d0e2e94578ee3e5bd6dcd67ac19891003ddb6e6e
saitovixtiyor <ixtyorsaitov@gmail.com>
2026-09-23 19:38:26 +0500

    update routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,6 +2,10 @@ import { lazy } from "react";
 import { PATHS } from "./paths";
 const DashboardPage = lazy(() => import("../pages/dashboard/page"));
 const NewsPage = lazy(() => import("../pages/news/page"));
+const TagsPage = lazy(() => import("../pages/tags/page"));
+const CategoriesPage = lazy(() => import("../pages/categories/page"));
+const AdminsPage = lazy(() => import("../pages/admins/page"));
+
 const NotFoundPage = lazy(() => import("../pages/not-found/page"));
 
 export const routes = [
@@ -12,6 +16,18 @@ export const routes = [
   {
     path: PATHS.NEWS,
     component: NewsPage,
+  },
+  {
+    path: PATHS.TAGS,
+    component: TagsPage,
+  },
+  {
+    path: PATHS.CATEGORIES,
+    component: CategoriesPage,
+  },
+  {
+    path: PATHS.ADMINS,
+    component: AdminsPage,
   },
   {
     component: NotFoundPage,
